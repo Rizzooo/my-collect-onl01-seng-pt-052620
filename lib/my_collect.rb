83c767578b6acc26a1338af3270caa41c2a3a733
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-04 22:44:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(languagues)
     i = 0 
     
   collection = []
-  words = languages.split(" ")
+  words = languages.split("")
   # cap_langs = languages.upcase
   while i < languages.length do
     words.upcase
