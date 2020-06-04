23125137c09d82081caaa47dcef3cb0462e62d35
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-04 22:36:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
+def my_collect(languagues)
+  if block_given?
+    i = 0 
+    
+  collection = []
+  while i < languages.length do
+    puts languagues.upcase
+  collection << yield(languages[i])
+  i += 1
+end
+collection
+  else
+    "No block!"
+end
+end
 
