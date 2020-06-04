166418570f3b1e3714dc7c5429468dde62854cb1
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-04 22:56:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,8 +5,8 @@ def my_collect(languages)
   collection = []
   words = languages.join
   while i < languages.length do
-    words.upcase == almost
-    "#{almost}".split
+    words.upcase = almost
+    #{almost}.split
   collection << yield(words[i])
   i += 1
   rewind = words.split(" ")
